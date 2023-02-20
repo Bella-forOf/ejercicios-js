bee333e9d99624466e0166c4eef327f43eeaba20
Bella Irene <bellagarcia1985@gmail.com>
2023-02-20 10:00:49 +0100

    ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+/* OBJETIVO: Dado un array, devuelve el array invertido,
+Input: [1, 2, 3]
+Output:  [3, 2, 1]
+ */
+
+let array1  = [1, 2, 3];
+let array2 =[];
+
+
+const arr1  = [1, 2, 3];
+console.log(arr1);
+const arr2 = arr1.reverse();
+console.log(arr2);
